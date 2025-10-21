ba012e8934a97c9f4b9ae05dbf62527f2497b392
wnshtm <2026_juyoung_kim@ksdream.or.kr>
2025-10-21 14:16:45 +0900

    수정1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-center">
-      <h1 className="text-2xl font-bold">미래사회변화주 프로젝트에 오신 것을 환영합니다</h1>
+      <h1 className="text-2xl font-bold">미래변화주도 프로젝트에 오신 것을 환영합니다</h1>
       <p className="mt-4">상단 메뉴를 통해 원하는 기능으로 이동하세요.</p>
     </div>
   );
